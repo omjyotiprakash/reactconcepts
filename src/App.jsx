6bd85c90552b3473d328a7239f7e74aa7206479e
Mprakash2002 <omjyotiprakash4@gmail.com>
2026-06-12 13:12:37 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Props_home from './topics/props/Props_home';
+import Conditional_Rendering from './topics/conditional_rendering/Conditional_Rendering';
 // import Jsx_Expression from './topics/jsx_expression/Jsx_Expression';
 // import Jsx_Rules from './topics/jsx_expression/Jsx_Rules';
 
@@ -9,7 +10,8 @@ const App = () => {
     <div>
       {/* <Jsx_Expression/> */}
       {/* <Jsx_Rules/> */}
-      <Props_home/>
+      {/* <Props_home/> */}
+      <Conditional_Rendering/>
     </div>
   )
 }
---
+++ src/topics/conditional_rendering/Conditional_Rendering.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Conditional_Rendering = () => {
+  return (
+    <div>
+      hii
+    </div>
+  )
+}
+
+export default Conditional_Rendering
