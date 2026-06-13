082097507f2a0facb0bbf12122f74647d78c5e20
Mprakash2002 <omjyotiprakash4@gmail.com>
2026-06-13 13:52:33 +0530

    Initial commit

--- src/topics/fragments/Fragments_Concept.jsx
+++ src/topics/fragments/Fragments_Concept.jsx
@@ -4,7 +4,9 @@ import React from 'react'
 const Fragments_Concept = () => {
   return (
     <div>
-      hii
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam deleniti quas quia nisi cupiditate assumenda at, ex ea eius deserunt eveniet obcaecati repudiandae vitae earum pariatur tenetur. Doloremque, error sint?</p>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nostrum reiciendis sequi! Saepe quos est veritatis a dignissimos, doloremque illo officia adipisci cum, quis porro itaque expedita totam accusamus at?</p>
     </div>
   )
 }
