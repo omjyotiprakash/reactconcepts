22d3e156330ae6532c0a4f3f79dafbbb2deb2c0a
Mprakash2002 <omjyotiprakash4@gmail.com>
2026-06-25 13:34:52 +0530

    Initial commit

--- src/topics/listandkeys/ListAndKeys.jsx
+++ src/topics/listandkeys/ListAndKeys.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const ListAndKeys = () => {
+  const arr = ['sundari', 'maala', 'sheela']
   return (
     <div>
-      ListAndKeys
+      <ol>
+        {
+          arr.map((ele,idx) => {
+            return <li className='list-decimal' key={idx}>{ele}</li>
+          })
+        }
+      </ol>
     </div>
   )
 }
