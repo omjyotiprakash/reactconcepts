67c58cc024a1ddee20e91e2893334df23d58a6e2
Mprakash2002 <omjyotiprakash4@gmail.com>
2026-06-25 14:03:58 +0530

    Initial commit

--- src/topics/uncontrolledform/UncontrolledForm.jsx
+++ src/topics/uncontrolledform/UncontrolledForm.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const UncontrolledForm = () => {
+
+  const usernameRef = useRef()
+  const ageRef = useRef()
+
+  const handleForm = (e) => {
+    e.preventDefault()
+    console.log(usernameRef.current.value)
+    usernameRef.current.value=""
+    console.log(ageRef.current.value)
+    ageRef.current.value=""
+  }
+
   return (
     <div>
-      UncontrolledForm
+      <h1>UncontrolledForm</h1>
+      <form onSubmit={handleForm}>
+        <input ref={usernameRef} type="text" placeholder="name" />
+        <input ref={ageRef} type="number" placeholder="age" />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
